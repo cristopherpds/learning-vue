ad5d827ebafab90deb84e2a0cb0b35c4dc51e3f5
Cristopher <josuepaiva7@gmail.com>
2021-10-11 13:35:30 -0300

    se creo los state para obtener y filtrar los personajes
    se creo las mutations para modificar los state
    
    se creo una actions para obtener los personajes de una API

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,27 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, payload){
+      state.characters = payload;
+    },
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload;
+    },
   },
   actions: {
+    async getCharacters({commit}){
+      try {
+        const res = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await res.json()
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   },
   modules: {
   }
